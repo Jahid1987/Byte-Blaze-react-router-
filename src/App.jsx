46322dfeacf05ae8a5655366c6136ad88be77e15
Jahid Anwer <jahid.englaa@gmail.com>
2024-03-21 14:00:07 +0600

    nav, theme functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Home from "./Pages/Home";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-3xl">this is test</p>
-      <button className="btn btn-primary">button</button>
+      <Home></Home>
     </>
   );
 }
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../Coponents/Nav";
+
+const Home = () => {
+  return (
+    <>
+      <Nav></Nav>
+      <h2>Home page</h2>
+    </>
+  );
+};
+
+export default Home;
